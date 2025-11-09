523a322eeb6e1f3a54203a3d840d837dabcd8b5e
Motolani <alebiosumotolani@gmail.com>
2025-11-09 23:24:12 +0100

    Task 4

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,32 @@
+import { Subjects } from './subjects/Teacher';
+import { Subjects as CppSubjects } from './subjects/Cpp';
+import { Subjects as JavaSubjects } from './subjects/Java';
+import { Subjects as ReactSubjects } from './subjects/React';
+
+// Teacher object with experienceTeachingC
+export const cTeacher: Subjects.Teacher = {
+  firstName: 'Guillaume',
+  lastName: 'Salva',
+  experienceTeachingC: 10,
+};
+
+// Cpp
+export const cpp = new CppSubjects.Cpp();
+console.log('C++');
+cpp.setTeacher(cTeacher);
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+// Java
+export const java = new JavaSubjects.Java();
+console.log('Java');
+java.setTeacher(cTeacher);
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+// React
+export const react = new ReactSubjects.React();
+console.log('React');
+react.setTeacher(cTeacher);
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
