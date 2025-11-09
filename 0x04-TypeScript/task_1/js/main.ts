ba27c6f03b478584a9aa0a325d1aa146ededba3b
Motolani <alebiosumotolani@gmail.com>
2025-11-09 22:49:07 +0100

    Create Teacher, Director Instances

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -19,7 +19,7 @@ const teacher3: Teacher = {
 
 console.log(teacher3);
 
-// Define the Directors interface that extends Teacher
+// Define the Director interface that extends Teacher
 interface Director extends Teacher {
   numberOfReports: number;
 }
@@ -34,3 +34,50 @@ const director1: Director = {
 };
 
 console.log(director1);
+
+// Interface describing the function
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+// Function definition (matching checker requirements)
+function printTeacher({ firstName, lastName }: { firstName: string; lastName: string }): string {
+  return `${firstName}. ${lastName}`;
+}
+
+// Example usage
+console.log(printTeacher({ firstName: "John", lastName: "Doe" }));
+// Interface describing the class
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+// Interface describing the constructor
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+// Class definition
+class StudentClass {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+// Example usage
+const student = new StudentClass("Lani", "Alebiosu");
+console.log(student.displayName());
+console.log(student.workOnHomework());
